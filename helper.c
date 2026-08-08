b81594d1393162f28607eed6cb809bd076af6e82
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-08-08 14:48:59 +0430

    oh sorry my bad

--- helper.c
+++
@@ -1,6 +0,0 @@
-#include <setjmp.h>
-#include <signal.h>
-
-static sigjmp_buf g_jmp_env;
-static void v_segfault_handler(int sig) { siglongjmp(g_jmp_env, 1); }
-static int safe_sigsetjmp() { return sigsetjmp(g_jmp_env, 1); }
