3853b8d671ee86ed7cec5c29847fb03c119aa9ce
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-08-18 14:39:49 +0430

    Enhance remote_call_arch with FP register support
    Updated remote_call_arch to handle floating point registers for different architectures.

--- helper.h
+++ helper.h
@@ -16,14 +16,36 @@
 #include <sys/user.h>
 #include <sys/uio.h>
 #include <sys/syscall.h>
-#include <elf.h>
+#include <linux/elf.h>
 
-#if defined(__aarch64__) || defined(__arm__)
-#include <asm/ptrace.h>
+#if defined(__x86_64__) || defined(__i386__)
+#include <sys/reg.h>
 #endif
 
 #ifndef NT_PRSTATUS
 #define NT_PRSTATUS 1
+#endif
+
+#ifndef NT_PRFPREG
+#define NT_PRFPREG 2
+#endif
+
+#ifndef NT_ARM_VFP
+#define NT_ARM_VFP 0x400
+#endif
+
+#if defined(__aarch64__)
+struct local_user_fpsimd_struct {
+    uint64_t vregs[32][2];
+    uint32_t fpsr;
+    uint32_t fpcr;
+    uint32_t __pad;
+};
+#elif defined(__arm__)
+struct local_user_vfp {
+    uint64_t fpregs[32];
+    uint32_t fpscr;
+};
 #endif
 
 #ifndef __NR_process_vm_readv
@@ -110,7 +132,7 @@ static inline uintptr_t get_remote_module_base(pid_t pid, const char *module_nam
     return base;
 }
 
-static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int argc, uintptr_t *argv) {
+static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int argc, uintptr_t *argv, int is_fp) {
     int status;
     if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) == -1) {
         return (uintptr_t)-1;
@@ -119,11 +141,25 @@ static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int arg
 
 #if defined(__aarch64__)
     struct user_pt_regs regs, orig_regs;
+    struct local_user_fpsimd_struct fpsimd, orig_fpsimd;
     struct iovec iov = { &regs, sizeof(regs) };
     struct iovec orig_iov = { &orig_regs, sizeof(orig_regs) };
+    struct iovec fp_iov = { &fpsimd, sizeof(fpsimd) };
+    struct iovec orig_fp_iov = { &orig_fpsimd, sizeof(orig_fpsimd) };
 
     if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &iov) == -1) goto fail;
     memcpy(&orig_regs, &regs, sizeof(regs));
+
+    int has_fp = (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRFPREG, &fp_iov) == 0);
+    if (has_fp) {
+        memcpy(&orig_fpsimd, &fpsimd, sizeof(fpsimd));
+        for (int i = 0; i < argc && i < 8; ++i) {
+            fpsimd.vregs[i][0] = argv[i];
+            fpsimd.vregs[i][1] = 0;
+        }
+        fp_iov.iov_len = sizeof(fpsimd);
+        ptrace(PTRACE_SETREGSET, pid, (void *)NT_PRFPREG, &fp_iov);
+    }
 
     uintptr_t sp = regs.sp;
     sp = (sp - 2048) & ~0xF;
@@ -150,14 +186,36 @@ static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int arg
     if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &iov) == -1) goto fail;
     uintptr_t ret_val = regs.regs[0];
 
+    if (has_fp) {
+        fp_iov.iov_len = sizeof(fpsimd);
+        if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRFPREG, &fp_iov) == 0) {
+            if (is_fp) {
+                ret_val = fpsimd.vregs[0][0];
+            }
+        }
+        orig_fp_iov.iov_len = sizeof(orig_fpsimd);
+        ptrace(PTRACE_SETREGSET, pid, (void *)NT_PRFPREG, &orig_fp_iov);
+    }
+
     ptrace(PTRACE_SETREGSET, pid, (void *)NT_PRSTATUS, &orig_iov);
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
     return ret_val;
 
 #elif defined(__x86_64__)
     struct user_regs_struct regs, orig_regs;
+    struct user_fpregs_struct fpregs, orig_fpregs;
+
     if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == -1) goto fail;
     memcpy(&orig_regs, &regs, sizeof(regs));
+
+    int has_fp = (ptrace(PTRACE_GETFPREGS, pid, NULL, &fpregs) == 0);
+    if (has_fp) {
+        memcpy(&orig_fpregs, &fpregs, sizeof(fpregs));
+        for (int i = 0; i < argc && i < 8; ++i) {
+            memcpy(&fpregs.xmm_space[i * 4], &argv[i], sizeof(uint64_t));
+        }
+        ptrace(PTRACE_SETFPREGS, pid, NULL, &fpregs);
+    }
 
     uintptr_t sp = regs.rsp;
     sp = (sp - 2048) & ~0xF;
@@ -193,17 +251,39 @@ static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int arg
     if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == -1) goto fail;
     uintptr_t ret_val = regs.rax;
 
+    if (has_fp) {
+        if (ptrace(PTRACE_GETFPREGS, pid, NULL, &fpregs) == 0) {
+            if (is_fp) {
+                memcpy(&ret_val, &fpregs.xmm_space[0], sizeof(uintptr_t));
+            }
+        }
+        ptrace(PTRACE_SETFPREGS, pid, NULL, &orig_fpregs);
+    }
+
     ptrace(PTRACE_SETREGS, pid, NULL, &orig_regs);
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
     return ret_val;
 
 #elif defined(__arm__)
     struct pt_regs regs, orig_regs;
+    struct local_user_vfp vfp, orig_vfp;
     struct iovec iov = { &regs, sizeof(regs) };
     struct iovec orig_iov = { &orig_regs, sizeof(orig_regs) };
+    struct iovec fp_iov = { &vfp, sizeof(vfp) };
+    struct iovec orig_fp_iov = { &orig_vfp, sizeof(orig_vfp) };
 
     if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &iov) == -1) goto fail;
     memcpy(&orig_regs, &regs, sizeof(regs));
+
+    int has_fp = (ptrace(PTRACE_GETREGSET, pid, (void *)NT_ARM_VFP, &fp_iov) == 0);
+    if (has_fp) {
+        memcpy(&orig_vfp, &vfp, sizeof(vfp));
+        for (int i = 0; i < argc && i < 8; ++i) {
+            vfp.fpregs[i] = argv[i];
+        }
+        fp_iov.iov_len = sizeof(vfp);
+        ptrace(PTRACE_SETREGSET, pid, (void *)NT_ARM_VFP, &fp_iov);
+    }
 
     uintptr_t sp = regs.ARM_sp;
     sp = (sp - 1024) & ~0x7;
@@ -229,6 +309,17 @@ static inline uintptr_t remote_call_arch(pid_t pid, uintptr_t func_addr, int arg
 
     if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_PRSTATUS, &iov) == -1) goto fail;
     uintptr_t ret_val = regs.ARM_r0;
+
+    if (has_fp) {
+        fp_iov.iov_len = sizeof(vfp);
+        if (ptrace(PTRACE_GETREGSET, pid, (void *)NT_ARM_VFP, &fp_iov) == 0) {
+            if (is_fp) {
+                ret_val = (uintptr_t)vfp.fpregs[0];
+            }
+        }
+        orig_fp_iov.iov_len = sizeof(orig_vfp);
+        ptrace(PTRACE_SETREGSET, pid, (void *)NT_ARM_VFP, &orig_fp_iov);
+    }
 
     ptrace(PTRACE_SETREGSET, pid, (void *)NT_PRSTATUS, &orig_iov);
     ptrace(PTRACE_DETACH, pid, NULL, NULL);
